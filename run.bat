23fd160c4baae8054887867df6ea31d8892e4e80
kingsley <281206960@qq.com>
2018-03-30 17:47:50 +0800

    2018.3.30 changing bat file's executing command and rebuild app's requirements

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 echo 正在准备运行爬虫
-py -2 processing_run.py
+python processing_run.py
 echo 爬虫运行完成 & pause
 exit
--- setup.bat
+++ setup.bat
@@ -1,6 +1,7 @@
 @echo off
 echo 正在安装python所需依赖包
-py -2 -m pip install -r requirement.txt
+set path=%path%;C:\Python27\Scripts;
+pip install -r requirement.txt
 echo 依赖包安装完成
 echo 请按任意键退出 & pause
 exit
